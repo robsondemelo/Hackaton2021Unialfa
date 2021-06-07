d73ea63d76ea47c4d4fab771affdd4d3cd880feb
Robson Melo <melodisouza@gmail.com>
2021-06-07 18:06:49 -0300

    Admin criando, listando e editando, baseado no codigo subsubmarino

---
+++ Admin/cadastros/veiculos.php
@@ -0,0 +1,195 @@
+<?php
+    if ( ! isset ( $_SESSION['submarino']['id'] ) ) exit;
+
+    $modelo = $anomodelo = $anofabricacao = $valor = $tipo = $fotoDestaque = $marca_id = $cor_id = $opcionais = NULL;
+    $usuario_id = ($_SESSION['submarino']['id']);
+
+    //verificando se pegou o id de usuario da sessao
+    //echo $usuario_id;
+
+
+    //select para edição
+    if ( ! empty ( $id ) ) {
+
+        //sql para recuperar os dados daquele id
+        $sql = "select * from veiculo where id = :id limit 1";
+        //pdo - preparar
+        $consulta = $pdo->prepare($sql);
+        //passar um parametro - id
+        $consulta->bindParam(':id', $id);
+        //executar o sql
+        $consulta->execute();
+
+        $dados = $consulta->fetch(PDO::FETCH_OBJ);
+
+        //recuperar os dados
+        $modelo = $dados->modelo;
+        $anomodelo = $dados->anomodelo;
+        $anofabricacao = $dados->anofabricacao;
+        $valor = formatarValorBR($dados->valor);
+        $tipo = $dados->tipo;
+        $fotoDestaque = $dados->fotoDestaque;
+        $marca_id = $dados->marca_id;
+        $cor_id = $dados->cor_id;
+        $opcionais = $dados->opcionais;
+
+    }
+
+?>
+<div class="card">
+	<div class="card-header">
+		<h3 class="float-left">Cadastro de Veiculos:</h3>
+		<div class="float-right">
+			<a href="cadastros/veiculos" class="btn btn-info">
+        		<i class="fas fa-file"></i> Novo
+        	</a>
+        	<a href="listar/veiculos" class="btn btn-info">
+        		<i class="fas fa-search"></i> Listar
+        	</a>
+		</div>
+	</div>
+	<div class="card-body">
+		<form name="formCadastro" method="post" action="salvar/veiculos" data-parsley-validate="" enctype="multipart/form-data">
+			
+			<div class="row">
+				<div class="col-12 col-md-2">
+					<label for="id">ID:</label>
+					<input type="text" name="id" id="id"
+					class="form-control" readonly
+					value="<?=$id?>">
+				</div>
+				<div class="col-12 col-md-10">
+					<label for="modelo">Modelo do Veiculo*:</label>
+					<input type="text" name="modelo"
+					id="modelo" class="form-control" required data-parsley-required-message="Digite o nome do veiculo"
+					value="<?=$modelo?>"  maxlength="200">
+				</div>
+				<div class="col-12 col-md-12">
+					<label for="opcionais">Opcionais*:</label>
+					<textarea name="opcionais" id="opcionais" class="form-control" required data-parsley-required-message="Digite os opcionais desse veiculo." rows="10"><?=$opcionais?></textarea>
+				</div>
+				<div class="col-12 col-md-4">
+					<label for="valor">Valor do veiculo*:</label>
+					<input type="text" name="valor" id="valor" class="form-control valor" required 
+					data-parsley-required-message="Digite o valor do veiculo." inputmode="numeric" value="<?=$valor?>">
+				</div>
+				<div class="col-12 col-md-2">
+					<label for="anofabricacao">Ano de Fabricação*:</label>
+					<input type="text" name="anofabricacao" id="anofabricacao" class="form-control" required 
+					data-parsley-required-message="Digite o ano de fabricação." inputmode="numeric" value="<?=$anofabricacao?>">
+				</div>
+				<div class="col-12 col-md-2">
+					<label for="anomodelo">Ano Modelo*:</label>
+					<input type="text" name="anomodelo" id="anomodelo" class="form-control" required 
+					data-parsley-required-message="Digite o ano modelo." inputmode="numeric" value="<?=$anomodelo?>">
+				</div>
+				<div class="col-12 col-md-4">
+					<?php
+
+						$required = ' required data-parsley-required-message="Selecione um arquivo" ';
+						$link = NULL;
+
+						//verificar se a imagem não esta em branco
+						if ( !empty ( $fotoDestaque ) ) {
+							//caminho para a imagem
+							$img = "../produtos/{$fotoDestaque}m.jpg";
+							//criar um link para abrir a imagem
+							$link = "<a href='{$img}' data-lightbox='foto' class='badge badge-success'>Abrir imagem</a>";
+							$required = NULL;
+
+						}
+
+					?>
+					<label for="fotoDestaque">Imagem (JPG)* <?=$link?>:</label>
+					<input type="file" name="fotoDestaque" 
+					id="fotoDestaque" class="form-control"
+					<?=$required?> accept="image/jpeg">
+				</div>
+				<div class="col-12 col-md-4">
+					<label for="marca_id">Selecione uma Marca*:</label>
+					<select name="marca_id" id="marca_id" class="form-control" required data-parsley-required-message="Selecione uma marca.">
+						<option value=""></option>
+						<?php
+						//selecionar todas as marcas
+						$sql = "select id, marca from marca order by marca";
+						$consulta = $pdo->prepare($sql);
+						$consulta->execute();
+
+						while ( $dados = $consulta->fetch(PDO::FETCH_OBJ) ) {
+
+							echo "<option value='{$dados->id}'>{$dados->marca}</option>";
+
+						}
+
+						?>
+					</select>
+				</div>
+				<div class="col-12 col-md-4">
+					<label for="cor_id">Selecione uma Cor*:</label>
+					<select name="cor_id" id="cor_id" class="form-control" required data-parsley-required-message="Selecione uma cor.">
+						<option value=""></option>
+						<?php
+						//selecionar todas as cores
+						$sql = "select id, cor from cor order by cor";
+						$consulta = $pdo->prepare($sql);
+						$consulta->execute();
+
+						while ( $dados = $consulta->fetch(PDO::FETCH_OBJ) ) {
+
+							echo "<option value='{$dados->id}'>{$dados->cor}</option>";
+
+						}
+
+						?>
+					</select>
+				</div>
+				<div class="col-12 col-md-4">
+					<label for="tipo">Tipo de Veículo:</label>
+					<select name="tipo" id="tipo" class="form-control" required data-parsley-required-message="Selecione uma opção.">
+						<option value="">Selecione</option>
+						<option value="novo">Novo</option>
+						<option value="seminovo">Semi-novo</option>
+					</select>
+				</div>
+			</div>
+
+			<button type="submit" class="btn btn-success float-right">
+				<i class="fas fa-check"></i> Salvar / Alterar
+			</button>
+
+			<br>
+			<p>
+				<small>* Obrigatório o preenchimento</small>
+			</p>
+		</form>
+	</div>
+</div>
+<script>
+	$(document).ready(function(){
+		$("#opcionais").summernote({
+			height: '200px',
+			lang: 'pt-BR',
+			toolbar: [
+	          ['style', ['style']],
+	          ['font', ['bold', 'underline', 'clear']],
+	          ['color', ['color']],
+	          ['para', ['ul', 'ol', 'paragraph']],
+	          ['table', ['table']],
+	          ['insert', ['link', 'picture', 'video']],
+	          ['view', ['codeview']]
+	        ]
+		});
+
+		$(".valor").maskMoney({
+			thousands: '.',
+			decimal: ','
+		});
+
+		//selecionar os campos ao editar
+		$("#marca_id").val(<?=$marca_id?>);
+		$("#cor_id").val(<?=$cor_id?>);
+		$("#tipo").val("<?=$tipo?>");
+	})
+</script>
+
+
---
+++ Admin/salvar/veiculos.php
@@ -0,0 +1,169 @@
+<?php
+    if ( ! isset ( $_SESSION['submarino']['id'] ) ) exit;
+
+    $usuario_id = ($_SESSION['submarino']['id']);
+   
+    if ( $_POST ) {
+
+    	//print_r( $_POST );
+
+    	//print_r( $_FILES );
+
+    	//recuperar os dados dados
+
+    	/*$id = "igor";
+
+    	echo "<p>O valor de id é $id</p>";
+
+    	$$id = "chimbinha";
+
+    	echo "<p>O valor de igor é $igor $id</p>";
+
+    	$$igor = "joelma";
+
+    	echo "<p>O valor de chimbinha é $chimbinha</p>";*/
+
+
+        include "libs/imagem.php";
+
+    	foreach ($_POST as $key => $value) {
+    		//echo "<p>{$key} - {$value}</p>";
+    		$$key = trim ( $value );
+    	}
+
+    	if ( empty ( $modelo ) ) {
+    		$titulo = "Erro ao salvar";
+    		$mensagem = "Preencha o campo modelo.";
+    		$icone = "error";
+
+    		mensagem($titulo, $mensagem, $icone);
+    	} else if ( empty ( $opcionais ) ) {
+ 
+     		mensagem("Erro ao salvar", 
+     			"Preencha os opcionais.", 
+     			"error");
+    	}
+
+    	/*echo formatarValor($valor);
+
+    	$v = "1.456,98";
+    	echo "<br>".formatarValor($v);
+
+    	echo "<br>".formatarValor('1.672,91');*/
+
+    	$valor = formatarValor($valor);
+    	
+        //programação para copiar uma imagem
+        //no insert envio da foto é obrigatório
+        //no update só se for selecionada uma nova imagem
+
+        //print_r ( $_FILES );
+
+        //se o id estiver em branco e o imagem tbém - erro
+        if ( ( empty ( $id ) ) and ( empty ( $_FILES['fotoDestaque']['name'] ) ) ) {
+            mensagem("Erro ao enviar imagem", 
+                "Selecione um arquivo JPG válido", 
+                "error");
+        } 
+
+        //se existir imagem - copia para o servidor
+        if ( !empty ( $_FILES['fotoDestaque']['name'] ) ) {
+            //calculo para saber quantos mb tem o arquivo
+            $tamanho = $_FILES['fotoDestaque']['size'];
+            $t = 8 * 1024 * 1024; //byte - kbyte - megabyte
+
+            $fotoDestaque = time();
+            $usuario = $_SESSION['submarino']['id'];
+
+            //definir um nome para a imagem
+            $fotoDestaque = "modelo_{$fotoDestaque}_{$usuario}";
+
+            //echo "<p>{$imagem}</p>"; exit;
+
+            //validar se é jpg
+            if ( $_FILES['fotoDestaque']['type'] != 'image/jpeg' ) {
+                mensagem("Erro ao enviar imagem", 
+                "O arquivo enviado não é um JPG válido, selecione um arquivo JPG", 
+                "error");
+            } else if ( $tamanho > $t ) {
+                mensagem("Erro ao enviar imagem", 
+                "O arquivo é muito grande e não pode ser enviado. Tente arquivos menores que 8 MB", 
+                "error");
+            } else if ( !copy ( $_FILES['fotoDestaque']['tmp_name'], '../produtos/'.$_FILES['fotoDestaque']['name'] ) ) {
+                mensagem("Erro ao enviar imagem", 
+                "Não foi possível copiar o arquivo para o servidor", 
+                "error");
+            }
+
+            //redimensionar a imagem
+            $pastaFotos = '../produtos/';
+            loadImg($pastaFotos.$_FILES['fotoDestaque']['name'], 
+                    $fotoDestaque, 
+                    $pastaFotos);
+
+        } //fim da verificação da foto
+
+        //se vai dar insert ou update
+        if ( empty ( $id ) ) {
+            $usuario_id = ($_SESSION['submarino']['id']);
+            $sql = "insert into veiculo values( NULL, :modelo, :anomodelo, :anofabricacao, :valor, :tipo, :fotoDestaque, :marca_id, :cor_id, :usuario_id, :opcionais )";
+            $consulta = $pdo->prepare($sql);
+            $consulta->bindParam(':modelo', $modelo);
+            $consulta->bindParam(':anomodelo', $anomodelo);
+            $consulta->bindParam(':anofabricacao', $anofabricacao);
+            $consulta->bindParam(':valor', $valor);
+            $consulta->bindParam(':tipo', $tipo);
+            $consulta->bindParam(':fotoDestaque', $fotoDestaque);
+            $consulta->bindParam(':marca_id', $marca_id);
+            $consulta->bindParam(':cor_id', $cor_id);
+            $consulta->bindParam(':usuario_id', $usuario_id);
+            $consulta->bindParam(':opcionais', $opcionais);
+
+        } else if ( empty ( $imagem ) ) {
+            $usuario_id = ($_SESSION['submarino']['id']);
+            $sql = "update veiculo set modelo = :modelo, anomodelo = :anomodelo, anofabricacao = :anofabricacao, valor = :valor, tipo = :tipo, marca_id = :marca_id, cor_id = :cor_id, usuario_id = :usuario_id,  opcionais = :opcionais where id = :id limit 1";
+            $consulta = $pdo->prepare($sql);
+            $consulta->bindParam(':modelo', $modelo);
+            $consulta->bindParam(':anomodelo', $anomodelo);
+            $consulta->bindParam(':anofabricacao', $anofabricacao);
+            $consulta->bindParam(':valor', $valor);
+            $consulta->bindParam(':tipo', $tipo);
+            $consulta->bindParam(':marca_id', $marca_id);
+            $consulta->bindParam(':cor_id', $cor_id);
+            $consulta->bindParam(':usuario_id', $usuario_id);
+            $consulta->bindParam(':opcionais', $opcionais);
+            $consulta->bindParam(':id', $id);
+
+        } else {
+            $usuario_id = ($_SESSION['submarino']['id']);
+            $sql = "update veiculo set modelo = :modelo, anomodelo = :anomodelo, anofabricacao = :anofabricacao, valor = :valor, tipo = :tipo, fotoDestaque = :fotoDestaque, marca_id = :marca_id, cor_id = :cor_id, usuario_id = :usuario_id,  opcionais = :opcionais where id = :id limit 1";
+            $consulta = $pdo->prepare($sql);
+            $consulta->bindParam(':modelo', $modelo);
+            $consulta->bindParam(':anomodelo', $anomodelo);
+            $consulta->bindParam(':anofabricacao', $anofabricacao);
+            $consulta->bindParam(':valor', $valor);
+            $consulta->bindParam(':tipo', $tipo);
+            $consulta->bindParam(':fotoDestaque', $fotoDestaque);
+            $consulta->bindParam(':marca_id', $marca_id);
+            $consulta->bindParam(':cor_id', $cor_id);
+            $consulta->bindParam(':usuario_id', $usuario_id);
+            $consulta->bindParam(':opcionais', $opcionais);
+            $consulta->bindParam(':id', $id);
+
+        }
+
+        //executar e verificar se foi salvo de verdade
+        if ( $consulta->execute() ) {
+            mensagem("OK", 
+                "Registro salvo/alterado com sucesso!", 
+                "ok");
+        } else {
+            echo $erro = $consulta->errorInfo()[2];
+
+            mensagem("Erro", 
+                "Erro ao salvar ou alterar registro", 
+                "error");
+        }
+
+
+    }
